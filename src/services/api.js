a9a153e20b7ef96c096b3db54cb6d241b5e61068
Muthu-A <muthuaspu@gmail.com>
2026-04-04 17:08:53 +0530

    Api url updated

--- src/services/api.js
+++ src/services/api.js
@@ -9,7 +9,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5050/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5050/api",
 });
 
 // ✅ Attach seasonId automatically
